ed83448d351d69b0c244a7480037ce5a3207990a
forifido <forif.ido@gmail.com>
2024-08-11 21:48:41 +0800

    v2.1.0

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/forifido/gomod-research
+module github.com/forifido/gomod-research/v2
 
 go 1.22
--- something.go
+++ something.go
@@ -3,10 +3,8 @@ package gomod_research
 import "fmt"
 
 func PrintVersion() {
-	fmt.Println(`v2.0.0: 
-	tag 修改为 v2, 但是模块版本没有修改为 github.com/forifdo/gomod-research/v2,
-	**不能**通过 require github.com/forifdo/gomod-research v2.0.0 导入模块,
-	**只能**通过 require github.com/forifdo/gomod-research <commit-id> 导入模块,
-	go mod tidy 后, 会自动补充为 require github.com/forifdo/gomod-research v1.0.0-0.<commit-time>-<commit-id>	
+	fmt.Println(`v2.1.0: 
+	模块修改为 github.com/forifido/gomod-research/v2
+	通过 require github.com/forifido/gomod-research/v2 v2.1.0
 	`)
 }
