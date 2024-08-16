835debed9337da6d79a30d7e56c23474a5aefe25
forifido <forif.ido@gmail.com>
2024-08-16 11:32:34 +0800

    v4.1.0

--- something.go
+++ something.go
@@ -3,5 +3,5 @@ package gomod_research
 import "fmt"
 
 func PrintVersion() {
-	fmt.Println(`v4.0.1, del go.mod and import with v4.0.1+incompatible`)
+	fmt.Println(`v4.1.0, del go.mod and import with v4.1.0+incompatible`)
 }
